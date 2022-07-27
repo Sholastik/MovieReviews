4187d38c0d1bcb9b171b8b76fb7fa30456ef7809
vyacheslavivanov <persikchicken@icloud.com>
2022-07-28 04:51:59 +0500

    Added openUrlOnClick to ReviewViewHolder

--- app/src/main/java/com/vyacheslavivanov/moviereviews/ui/util/BindingAdapters.kt
+++ app/src/main/java/com/vyacheslavivanov/moviereviews/ui/util/BindingAdapters.kt
@@ -1,9 +1,13 @@
 package com.vyacheslavivanov.moviereviews.ui.util
 
+import android.content.Intent
+import android.net.Uri
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.squareup.picasso.Picasso
 import jp.wasabeef.picasso.transformations.RoundedCornersTransformation
+
 
 @BindingAdapter("imageUrl")
 fun bindImageUrl(view: ImageView, imageUrl: String) {
@@ -13,4 +17,16 @@ fun bindImageUrl(view: ImageView, imageUrl: String) {
         .centerCrop()
         .transform(RoundedCornersTransformation(16, 8))
         .into(view)
+}
+
+@BindingAdapter("openUrlOnClick")
+fun bindOpenUrlOnClick(view: View, url: String) {
+    view.setOnClickListener {
+        it.context.startActivity(
+            Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(url)
+            )
+        )
+    }
 }
