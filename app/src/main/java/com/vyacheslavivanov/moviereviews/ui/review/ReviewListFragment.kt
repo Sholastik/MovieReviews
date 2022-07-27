7852764e48effc888ff2e53e301cb28041215187
vyacheslavivanov <persikchicken@icloud.com>
2022-07-28 04:14:34 +0500

    Added error snackbar Refactored code

--- app/src/main/java/com/vyacheslavivanov/moviereviews/ui/review/ReviewListFragment.kt
+++ app/src/main/java/com/vyacheslavivanov/moviereviews/ui/review/ReviewListFragment.kt
@@ -10,8 +10,11 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.paging.CombinedLoadStates
 import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.vyacheslavivanov.moviereviews.R
 import com.vyacheslavivanov.moviereviews.databinding.FragmentReviewListBinding
 import com.vyacheslavivanov.moviereviews.ui.review.adapter.ReviewAdapter
 import com.vyacheslavivanov.moviereviews.viewmodel.review.ReviewListViewModel
@@ -30,41 +33,65 @@ class ReviewListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentReviewListBinding.inflate(layoutInflater, container, false)
+        binding.bindAdapter(ReviewAdapter())
 
-        val adapter = ReviewAdapter()
+        return binding.root
+    }
 
-        binding.bindAdapter(adapter)
+    private fun showErrorSnackbar(error: Throwable, onClick: () -> Unit) {
+        Snackbar.make(
+            binding.root,
+            error.toString(),
+            Snackbar.LENGTH_INDEFINITE
+        ).setAction(R.string.error_snackbar_title) {
+            onClick()
+        }.show()
+    }
 
+    private fun getError(states: CombinedLoadStates): LoadState.Error? {
+        return states.source.append as? LoadState.Error
+            ?: states.source.prepend as? LoadState.Error
+            ?: states.append as? LoadState.Error
+            ?: states.prepend as? LoadState.Error
+    }
+
+    private fun FragmentReviewListBinding.bindAdapter(
+        reviewAdapter: ReviewAdapter
+    ) {
         viewLifecycleOwner.run {
             lifecycleScope.launch {
+                // May be paused, but still visible (multi window app)
                 repeatOnLifecycle(Lifecycle.State.STARTED) {
                     viewModel.reviewFlow.collectLatest {
-                        adapter.submitData(it)
+                        reviewAdapter.submitData(it)
                     }
                 }
             }
 
             lifecycleScope.launch {
                 repeatOnLifecycle(Lifecycle.State.STARTED) {
-                    adapter.loadStateFlow.collectLatest {
-                        binding.prependProgress.isVisible = it.source.prepend is LoadState.Loading
-                        binding.appendProgress.isVisible = it.source.append is LoadState.Loading
+                    reviewAdapter.loadStateFlow.collectLatest {
+                        // Show progress bar on initial load
+
+                        val isListEmpty =
+                            it.refresh is LoadState.NotLoading && reviewAdapter.itemCount == 0
+                        binding.progressBar.isVisible = isListEmpty
+
+                        binding.appendProgress.isVisible = it.append is LoadState.Loading
+
+                        getError(it)?.let { errorState ->
+                            showErrorSnackbar(errorState.error) {
+                                reviewAdapter.retry()
+                            }
+                        }
                     }
                 }
             }
         }
 
-        return binding.root
-    }
-
-    companion object {
-        private fun FragmentReviewListBinding.bindAdapter(
-            reviewAdapter: ReviewAdapter
-        ) {
-            recyclerView.apply {
-                adapter = reviewAdapter
-                layoutManager = LinearLayoutManager(context)
-            }
+        recyclerView.apply {
+            adapter = reviewAdapter
+            layoutManager = LinearLayoutManager(context)
         }
     }
 }
