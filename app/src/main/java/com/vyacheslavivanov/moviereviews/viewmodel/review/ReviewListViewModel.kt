979115833875386c889dd4df303e191a228a71e4
vyacheslavivanov <persikchicken@icloud.com>
2022-07-28 02:22:06 +0500

    Created ReviewListViewModel

---
+++ app/src/main/java/com/vyacheslavivanov/moviereviews/viewmodel/review/ReviewListViewModel.kt
@@ -0,0 +1,27 @@
+package com.vyacheslavivanov.moviereviews.viewmodel.review
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import com.vyacheslavivanov.moviereviews.data.review.ReviewRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ReviewListViewModel @Inject constructor(
+    reviewRepository: ReviewRepository
+) : ViewModel() {
+    val reviewFlow = Pager(
+        config = PagingConfig(
+            pageSize = PagingConfig.MAX_SIZE_UNBOUNDED,
+            enablePlaceholders = false
+        ),
+        pagingSourceFactory = {
+            reviewRepository.pagingSource
+        }
+    )
+        .flow
+        .cachedIn(viewModelScope)
+}
