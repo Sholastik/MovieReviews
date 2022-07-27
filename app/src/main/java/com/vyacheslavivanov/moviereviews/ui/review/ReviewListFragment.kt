c30be06240118cb5a5a613ead07c3f660fd6ea50
vyacheslavivanov <persikchicken@icloud.com>
2022-07-28 02:59:35 +0500

    Bound ReviewAdapter to RecyclerView

--- app/src/main/java/com/vyacheslavivanov/moviereviews/ui/review/ReviewListFragment.kt
+++ app/src/main/java/com/vyacheslavivanov/moviereviews/ui/review/ReviewListFragment.kt
@@ -5,12 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.vyacheslavivanov.moviereviews.databinding.FragmentReviewListBinding
+import com.vyacheslavivanov.moviereviews.ui.review.adapter.ReviewAdapter
+import com.vyacheslavivanov.moviereviews.viewmodel.review.ReviewListViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ReviewListFragment : Fragment() {
     private lateinit var binding: FragmentReviewListBinding
+    private val viewModel: ReviewListViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +29,31 @@ class ReviewListFragment : Fragment() {
     ): View {
         binding = FragmentReviewListBinding.inflate(layoutInflater, container, false)
 
+        val adapter = ReviewAdapter()
+
+        binding.bindAdapter(adapter)
+
+        viewLifecycleOwner.run {
+            lifecycleScope.launch {
+                repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    viewModel.reviewFlow.collectLatest {
+                        adapter.submitData(it)
+                    }
+                }
+            }
+        }
+
         return binding.root
+    }
+
+    companion object {
+        private fun FragmentReviewListBinding.bindAdapter(
+            reviewAdapter: ReviewAdapter
+        ) {
+            recyclerView.apply {
+                adapter = reviewAdapter
+                layoutManager = LinearLayoutManager(context)
+            }
+        }
     }
 }
