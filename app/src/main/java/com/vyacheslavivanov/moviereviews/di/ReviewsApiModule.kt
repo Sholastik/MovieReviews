e639b34485908998dba46d1497c20eded73999e2
vyacheslavivanov <persikchicken@icloud.com>
2022-07-28 01:01:49 +0500

    Added OkHttpClient provider to ReviewsApiModule

--- app/src/main/java/com/vyacheslavivanov/moviereviews/di/ReviewsApiModule.kt
+++ app/src/main/java/com/vyacheslavivanov/moviereviews/di/ReviewsApiModule.kt
@@ -1,8 +1,12 @@
 package com.vyacheslavivanov.moviereviews.di
 
+import com.vyacheslavivanov.moviereviews.BuildConfig
 import dagger.Module
+import dagger.Provides
+import dagger.Reusable
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import okhttp3.OkHttpClient
 import javax.inject.Qualifier
 
 @Module
@@ -11,4 +15,18 @@ object ReviewsApiModule {
     @Qualifier
     @Retention(AnnotationRetention.BINARY)
     internal annotation class ReviewsApi
+
+    @ReviewsApi
+    @Provides
+    @Reusable
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor {
+                it.proceed(
+                    it.request()
+                        .newBuilder()
+                        .header("api-key", BuildConfig.API_KEY)
+                        .build()
+                )
+            }.build()
 }
