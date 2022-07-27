04b4e6b42e3a06bb45ec33941c801aa4ae5c3d2a
vyacheslavivanov <persikchicken@icloud.com>
2022-07-27 23:57:13 +0500

    Made MainActivity use ViewBinding

--- app/src/main/java/com/vyacheslavivanov/moviereviews/MainActivity.kt
+++ app/src/main/java/com/vyacheslavivanov/moviereviews/MainActivity.kt
@@ -3,12 +3,18 @@ package com.vyacheslavivanov.moviereviews
 import android.app.Activity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.vyacheslavivanov.moviereviews.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen() // SplashScreen won't show when run via Android Studio: https://issuetracker.google.com/issues/205021357
+    private lateinit var binding: ActivityMainBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen() // Should be run before super
+        // SplashScreen won't show when run via Android Studio: https://issuetracker.google.com/issues/205021357
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
