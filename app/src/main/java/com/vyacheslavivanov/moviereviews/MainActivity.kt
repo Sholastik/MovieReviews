37d02a383ef64c2e8c0ab8397682212a8086531d
vyacheslavivanov <persikchicken@icloud.com>
2022-07-27 23:40:42 +0500

    Set up splash screen

--- app/src/main/java/com/vyacheslavivanov/moviereviews/MainActivity.kt
+++ app/src/main/java/com/vyacheslavivanov/moviereviews/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.vyacheslavivanov.moviereviews
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen() // SplashScreen won't show when run via Android Studio: https://issuetracker.google.com/issues/205021357
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
