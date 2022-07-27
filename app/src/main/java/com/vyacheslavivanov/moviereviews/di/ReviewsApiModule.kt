9e01906db27857157814ec0a8995fcb8779cce5f
vyacheslavivanov <persikchicken@icloud.com>
2022-07-28 01:57:51 +0500

    Changed ReviewsApiModule installation component to FragmentComponent

--- app/src/main/java/com/vyacheslavivanov/moviereviews/di/ReviewsApiModule.kt
+++ app/src/main/java/com/vyacheslavivanov/moviereviews/di/ReviewsApiModule.kt
@@ -6,7 +6,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.Reusable
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.components.FragmentComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -14,8 +14,8 @@ import retrofit2.create
 import javax.inject.Qualifier
 
 @Module
-@InstallIn(ViewModelComponent::class)
-object ReviewsApiModule {
+@InstallIn(FragmentComponent::class)
+abstract class ReviewsApiModule {
     @Qualifier
     @Retention(AnnotationRetention.BINARY)
     internal annotation class ReviewsApi
