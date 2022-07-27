e20c623690e8815f62e339b4a8d7f3103bc325eb
vyacheslavivanov <persikchicken@icloud.com>
2022-07-28 04:41:15 +0500

    Updated libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,8 +99,8 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.10")
+    implementation("com.squareup.picasso:picasso:2.71828")
     implementation("jp.wasabeef:picasso-transformations:2.4.0")
     implementation("androidx.paging:paging-runtime:3.1.1")
 
