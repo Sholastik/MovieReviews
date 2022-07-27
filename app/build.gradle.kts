ea13659cdcf45f3cd9c95903a887e3f3cc0282cb
vyacheslavivanov <persikchicken@icloud.com>
2022-07-27 23:01:39 +0500

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+val versionMajor = 1
+val versionMinor = 0
+val versionPatch = 0
+
+android {
+    namespace = "com.vyacheslavivanov.moviereviews"
+
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.vyacheslavivanov.moviereviews"
+
+        minSdk = 21
+        targetSdk = 32
+
+        versionCode = versionMajor * 10000 + versionMinor * 100 + versionPatch
+        versionName = "${versionMajor}.${versionMinor}.${versionPatch}"
+
+        setProperty("archivesBaseName", "$applicationId-$versionName")
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        getByName("debug") {
+            ext["alwaysUpdateBuildId"] = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.appcompat:appcompat:1.4.2")
+    implementation("com.google.android.material:material:1.6.1")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
