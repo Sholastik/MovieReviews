d7ad718e625bb44ede063d74ea9789902dcdbcee
vyacheslavivanov <persikchicken@icloud.com>
2022-07-28 01:23:42 +0500

    Created ReviewPagingSource

---
+++ app/src/main/java/com/vyacheslavivanov/moviereviews/api/source/reviews/ReviewPagingSource.kt
@@ -0,0 +1,11 @@
+package com.vyacheslavivanov.moviereviews.api.source.reviews
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.vyacheslavivanov.moviereviews.data.reviews.Review
+
+abstract class ReviewPagingSource : PagingSource<Int, Review>() {
+    abstract override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Review>
+
+    abstract override fun getRefreshKey(state: PagingState<Int, Review>): Int
+}
